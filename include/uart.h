87cba4e2bd68b065d8ed48107bf7385f51b40966
Ryan Indrananda <ryan.indrananda@gmail.com>
2023-10-12 15:41:29 +1000

    UART initialisation

---
+++ include/uart.h
@@ -0,0 +1,6 @@
+
+#include <stdint.h>
+
+void uart_init(void);
+void uart_putc(char c);
+void uart_puts(char* string);
---
+++ src/uart.c
@@ -0,0 +1,31 @@
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+// typedef enum {
+//     INIT_SYM,
+//     SYM_TWO,
+//     SYM_THREE,
+//     SYM_FOUR
+// }
+
+void uart_init(void) { // EXT6, TUT9
+    PORTB.DIRSET = PIN2_bm;                             // Enable PB2 as output (USART0 TXD)
+    USART0.BAUD = 1389;                                 // 9600 baud @ 3.3 MHz - 8N1
+    USART0.CTRLA = USART_RXCIE_bm; // | USART_DREIE_bm; // Enable RX interrupts
+    USART0.CTRLB = USART_RXEN_bm | USART_TXEN_bm;       // Enable Tx/Rx
+}
+
+void uart_putc(uint8_t c){
+    while (!(USART0.STATUS & USART_DREIF_bm));   //wait for TXDATA empty
+    USART0.TXDATAL = c; 
+}
+
+void uart_puts(char* string) {
+    const char *ptr = string;
+    while (*ptr != '\0') {
+        uart_putc(*ptr);
+        ptr++;
+    }
+}
+
