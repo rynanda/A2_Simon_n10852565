5187240530948eeff127523f4f2de9e129a271a8
Ryan Indrananda <ryan.indrananda@gmail.com>
2023-10-15 19:09:39 +1000

    Cleaned up spi code

--- src/spi.c
+++ src/spi.c
@@ -5,14 +5,14 @@
 void spi_init(void) {
     cli();
     PORTMUX.SPIROUTEA = PORTMUX_SPI0_ALT1_gc;   // SPI pins on PC0-3
-    PORTC.DIR |= (PIN0_bm | PIN2_bm);           // Set SCK and MOSI as outputs
+    PORTC.DIRSET = PIN0_bm | PIN2_bm;           // Set SCK and MOSI as outputs
 
     SPI0.CTRLA = SPI_MASTER_bm;                 // Master, /4 prescaler, MSB first
-    SPI0_CTRLB = SPI_SSD_bm;                    // Mode 0, client select disable, unbuffered
-    SPI0_CTRLA |= SPI_ENABLE_bm;                // Enable
+    SPI0.CTRLB = SPI_SSD_bm;                    // Mode 0, client select disable, unbuffered
+    SPI0.CTRLA |= SPI_ENABLE_bm;                // Enable
 
-    PORTA_OUTCLR = PIN1_bm;                     // Set DISP LATCH low initially
-    PORTA_DIR |= PIN1_bm;                       // Set DISP LATCH as output
+    PORTA.OUTCLR = PIN1_bm;                     // Set DISP LATCH low initially
+    PORTA.DIRSET = PIN1_bm;                     // Set DISP LATCH as output
     SPI0.INTCTRL = SPI_IE_bm;                   // Enable SPI interrupt
     sei();
 }
@@ -22,6 +22,6 @@ void spi_write(uint8_t b) {
 }
 
 ISR(SPI0_INT_vect) {
-    PORTA_OUTSET = PIN1_bm;                     // Drive PA1 (DISP LATCH) high
-    SPI0_INTFLAGS ^= PIN7_bm;                   // Toggle off IF (bit 7) in SPI0.INTFLAGS to handle interrupt
+    PORTA.OUTSET = PIN1_bm;                     // Drive PA1 (DISP LATCH) high
+    SPI0.INTFLAGS ^= SPI_IF_bm;                   // Toggle off IF (bit 7) in SPI0.INTFLAGS to handle interrupt
 }
