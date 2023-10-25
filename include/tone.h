be1b702968325b68334728b1d1806af7951758d6
Ryan Indrananda <ryan.indrananda@gmail.com>
2023-10-26 01:21:29 +1000

    Changed buzzer to tone

--- include/tone.h
+++ include/tone.h
@@ -2,10 +2,10 @@
 // Include headers
 #include <stdint.h>
 
-// Function prototypes for buzzer features
-void buzzer_on(const uint8_t tone);
-void buzzer_off(void);
-void buzzer_success_fail(void);
+// Function prototypes for buzzer and display features
+void tone_on(const uint8_t tone);
+void tone_off(void);
+void tone_success_fail(void);
 void inc_octave(void);
 void dec_octave(void);
 void reset_octave(void);
